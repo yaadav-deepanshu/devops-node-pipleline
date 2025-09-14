f4ff1389407d8918715c4ccb75d41d2995b775e3
Deepanshu Yadav <yaadav.deepanshu@gmail.com>
2025-09-14 15:54:12 +0530

    chore(terraform): add variables and provider configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,15 @@
-variable "region" { default = "us-east-1" }
-variable "app_name" { default = "nodejs-logo-server" }
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "app_name" {
+  default = "nodejs-logo-server"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0e86e20dae9224db8" # Ubuntu 22.04 LTS in us-east-1
+}
