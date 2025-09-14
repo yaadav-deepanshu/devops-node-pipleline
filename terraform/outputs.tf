48a6b35d572554f0b0ab3c1fdbdf677da9129f24
Deepanshu Yadav <yaadav.deepanshu@gmail.com>
2025-09-14 15:55:16 +0530

    chore(terraform): add outputs for ALB, ECR repo, and Jenkins public IP

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,2 +1,11 @@
-output "alb_url" { value = aws_lb.app_lb.dns_name }
-output "ecr_repo_url" { value = aws_ecr_repository.app.repository_url }
+output "alb_url" {
+  value = aws_lb.app_lb.dns_name
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
