ec0a2adbaf87103bd4241c655311326828dd1da7
Deepanshu Yadav <yaadav.deepanshu@gmail.com>
2025-09-14 17:02:21 +0530

    chore(terraform): add variables for region, AMI, instance type, key pair, VPC, and subnet

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,14 +2,22 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "app_name" {
-  default = "nodejs-logo-server"
+variable "ami_id" {
+  default = "ami-0e86e20dae9224db8" # Ubuntu 24.04 LTS
 }
 
 variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "ami_id" {
-  default = "ami-0e86e20dae9224db8" # Ubuntu 22.04 LTS in us-east-1
+variable "key_name" {
+  default = "jenkins-key"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
 }
